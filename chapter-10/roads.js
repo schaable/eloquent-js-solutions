a446ae4408911893f7d0a2c28d1702d3f30f99bc
Luis Schaab <schaable@gmail.com>
2022-06-03 01:19:29 -0300

    Chapter 10

---
+++ chapter-10/roads.js
@@ -0,0 +1,23 @@
+const { buildGraph } = require('./graph');
+
+const roads = [
+  "Alice's House-Bob's House",
+  "Alice's House-Cabin",
+  "Alice's House-Post Office",
+  "Bob's House-Town Hall",
+  "Daria's House-Ernie's House",
+  "Daria's House-Town Hall",
+  "Ernie's House-Grete's House",
+  "Grete's House-Farm",
+  "Grete's House-Shop",
+  'Marketplace-Farm',
+  'Marketplace-Post Office',
+  'Marketplace-Shop',
+  'Marketplace-Town Hall',
+  'Shop-Town Hall',
+].map((road) => road.split('-'));
+
+// you can also reference exports directly, since it's passed down to the module wrapper
+module.exports = {
+  roadGraph: buildGraph(roads),
+};
