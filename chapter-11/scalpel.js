450ac19b1627b55b794cd337b4588f3bceb55dc1
Luis Schaab <schaable@gmail.com>
2022-06-07 23:41:36 -0300

    Chapter 11 - Scalpel location

---
+++ chapter-11/scalpel.js
@@ -0,0 +1,37 @@
+import { expect } from 'chai';
+import { anyStorage } from './async.js';
+import { bigOak } from './crow-tech.js';
+
+async function locateScalpel(nest) {
+  let previousScalpelLocation = '';
+  let scalpelLocation = nest.name;
+
+  while (scalpelLocation !== previousScalpelLocation) {
+    previousScalpelLocation = scalpelLocation;
+    scalpelLocation = await anyStorage(nest, scalpelLocation, 'scalpel');
+  }
+
+  return scalpelLocation;
+}
+
+function locateScalpel2(nest) {
+  function find(scalpelLocation, previousScalpelLocation) {
+    if (scalpelLocation === previousScalpelLocation) {
+      return scalpelLocation;
+    }
+
+    return anyStorage(nest, scalpelLocation, 'scalpel').then((newScalpelLocation) =>
+      find(newScalpelLocation, scalpelLocation)
+    );
+  }
+
+  return find(nest.name);
+}
+
+locateScalpel(bigOak).then((scalpelLocation) => {
+  expect(scalpelLocation).to.equal('Butcher Shop');
+});
+
+locateScalpel2(bigOak).then((scalpelLocation) => {
+  expect(scalpelLocation).to.equal('Butcher Shop');
+});
